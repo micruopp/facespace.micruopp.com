5d841ea1ffb88aadb89bda73f51700ef12871a3c
Michal Ruopp <micruopp@gmail.com>
2020-05-06 02:04:38 -0500

    testing sockets...

--- facespace/facespace/websocket.py
+++ facespace/facespace/websocket.py
@@ -1,30 +1,19 @@
 async def websocket_application(scope, receive, send):
-
-	async def handleConnect():
-		response = { 'type': 'websocket.accept' }
-		await send(response)
-
-	async def handleDisconnect():
-		return
-
-	async def handleReceive():
-		if event['text'] == 'ping':
-			response = {
-				'type': 'websocket.send',
-				'text': 'pong!'
-			}
-			await send(response)
-
 		while True:
 			event = await receive()
 
 			if event['type'] == 'websocket.connect':
-				handleConnect()
+				response = { 'type': 'websocket.accept' }
+				await send(response)
 				
 			if event['type'] == 'websocket.disconnect':
-				handleDisconnect()
 				break
 
 			if event['type'] == 'websocket.receive':
-				handleReceive()
+				if event['text'] == 'ping':
+					response = {
+						'type': 'websocket.send',
+						'text': 'pong!'
+					}
+					await send(response)
 			
